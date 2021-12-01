6a519d2cdbc506cf5ec96cf029f0141676008cf2
Aldo Subroto <93777399+aldosubroto@users.noreply.github.com>
2021-12-01 08:28:24 +0700

    Latihan 7.6

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-// membuat variabel 
-var http = require('http');
+// install dulu npm install express --save Latihan 7.3 dan 7.4-7.5
+var http = require('http')
+var express = require('express')
+var app =express()
 
-// membuat server object 
-http.createServer( (req, res)=>{
-   if(req.url =='/home')
-        res.write('<p>Konten Home</p>')
-    else if(req.url =='/product')
-        res.write('<p>Konten Product</p>')
-     else if(req.url =='/order')
-        res.write('<p>Konten Order</p>')
-     else if(req.url =='/')
-        res.write('<p>Pengembangan Aplikasi Web dengan Nodejs</p>')
-    else 
-        res.write('<h1>404</h1>')
-    // menuliskan respon kepada client 
+app.get('/home', (req,res)=>{
+    res.end('Home')
+})
+app.post('/about', (req,res)=>{
+    res.end('About')
+})
+app.listen(8000, ()=>{console.log('Server is runing on port 8000')})
 
-// memberhentikan respon 
-    res.end();
-}).listen(8000, ()=>{
-    console.log('Server is running on port 8000')
-});
+
+
+//========================================================================================
+// // membuat variabel 
+// var http = require('http');
+
+// // membuat server object 
+// http.createServer( (req, res)=>{
+//    if(req.url =='/home')
+//         res.write('<p>Konten Home</p>')
+//     else if(req.url =='/product')
+//         res.write('<p>Konten Product</p>')
+//      else if(req.url =='/order')
+//         res.write('<p>Konten Order</p>')
+//      else if(req.url =='/')
+//         res.write('<p>Pengembangan Aplikasi Web dengan Nodejs</p>')
+//     else 
+//         res.write('<h1>404</h1>')
+//     // menuliskan respon kepada client 
+
+// // memberhentikan respon 
+//     res.end();
+// }).listen(8000, ()=>{
+//     console.log('Server is running on port 8000')
+// });
